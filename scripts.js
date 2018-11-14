be62886a8cdad87bf62767ca4b2c1717845ac653
Númi Steinn Baldursson <numisteinn@htgi-dw0358.rhi.hi.is>
2018-11-14 10:16:55 +0000

    laga apis linkinn, http í https

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // const API_URL = '/example.json?domain=';
-const API_URL = 'http://apis.is/isnic?domain=';
+const API_URL = 'https://apis.is/isnic?domain=';
 const domains = document.querySelector('.domains');
 /**
  * Leit að lénum á Íslandi gegnum apis.is
